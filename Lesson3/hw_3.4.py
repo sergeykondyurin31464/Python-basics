c7767bfc05d122706c3d130c516631d1533f09d0
sergeykondyurin31464 <ksu1972@list.ru>
2020-11-27 16:27:37 +0500

    Задание к 3 уроку

---
+++ Lesson3/hw_3.4.py
@@ -0,0 +1,8 @@
+# Возведение числа в степень с отрицательным показателем
+print('enter x')
+x = float(input())
+print('enter y')
+y = float(input())
+def my_func(x, y):
+   return 1/x**y
+print(my_func(x, y))
