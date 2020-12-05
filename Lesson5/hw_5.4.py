27333f28f860a115c3895d251f08d4ac2830a46e
sergeykondyurin31464 <ksu1972@list.ru>
2020-12-05 16:28:07 +0500

    Задание к 5 уроку

---
+++ Lesson5/hw_5.4.py
@@ -0,0 +1,20 @@
+# Необходимо написать программу,
+# открывающую файл на чтение и считывающую построчно данные.
+# При этом английские числительные должны заменяться на русские.
+# Новый блок строк должен записываться в новый текстовый файл.
+
+with open('5.4.txt', encoding='utf-8') as f:
+    lines = f.readlines()
+
+
+with open('5.4.1.txt', 'w', encoding='utf-8') as f:
+    for line in lines:
+        if '1' in line:
+            line = line.replace('One', 'Один')
+        elif '2' in line:
+            line = line.replace('Two', 'Два')
+        elif '3' in line:
+            line = line.replace('Three', 'Три')
+        elif '4' in line:
+            line = line.replace('Four', 'Четыре')
+        f.write(line)
